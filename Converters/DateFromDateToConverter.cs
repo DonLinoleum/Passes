244b1888bcfe5d9b1ce32265907c6b78c3a5606c
catosaurrex@gmail.com <catosaurrex@gmail.com>
2025-06-12 03:46:15 +0900

    progress

---
+++ Converters/DateFromDateToConverter.cs
@@ -0,0 +1,30 @@
+﻿using Passes.Models.PassDetail;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passes.Converters
+{
+    class DateFromDateToConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is PassDetailModel pass && pass is not null)
+            {
+                if (!String.IsNullOrEmpty(pass.date_from) && !String.IsNullOrEmpty(pass.date_to))
+                    return $"с {pass.date_from} по {pass.date_to}";
+                if (!String.IsNullOrEmpty(pass.date_from))
+                    return pass.date_from;
+            }
+            return "";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
